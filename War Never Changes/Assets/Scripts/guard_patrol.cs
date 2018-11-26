4deff28baa7933c0a95e4335fa306efe67c8d2c4
Dylberto <dylang565@gmail.com>
2018-11-26 20:51:24 +0000

    Guard patrol

---
+++ War Never Changes/Assets/Scripts/guard_patrol.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guard_patrol : MonoBehaviour {
+
+
+    public float MaxSpeed;
+    public Transform LeftPos, RightPos;
+    public float flip = 180;
+    private Rigidbody _myRB;
+    private bool _isFacingLeft;
+
+    void Start()
+    {
+        _myRB = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        _myRB.velocity = new Vector3(MaxSpeed * transform.localScale.x, _myRB.velocity.y, _myRB.velocity.z);
+
+        if (transform.position.x > RightPos.position.x && !_isFacingLeft)
+        {
+            Flip();
+        }
+
+        if (transform.position.x < LeftPos.position.x && _isFacingLeft)
+        {
+            Flip();
+        }
+
+
+    }
+
+    void Flip()
+    {
+        Vector3 myScale = transform.localScale;
+        myScale.x *= -1;
+        transform.localScale = myScale;
+        _isFacingLeft = !_isFacingLeft;
+
+        transform.Rotate(new Vector3(180, 0, 0));
+
+    }
+
+}
