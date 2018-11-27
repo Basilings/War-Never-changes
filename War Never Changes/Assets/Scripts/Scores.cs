70be6126d0e5a4ee5c314460b54abc49c3805acc
Dylberto <dylang565@gmail.com>
2018-11-27 12:30:07 +0000

    Pick-ups working (alpha)

---
+++ War Never Changes/Assets/Scripts/Scores.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scores : MonoBehaviour
+{
+    public int MEDS = 0;
+
+
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(60, 20, 200, 20), "Meds: " + MEDS);
+    }
+
+}
+
---
+++ War Never Changes/Assets/Scripts/meds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Scores>().MEDS++;
+            //add 1 to point
+            Destroy(gameObject);
+        }
+    }
+}
