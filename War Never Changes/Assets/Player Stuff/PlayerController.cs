a158ffa3be3021668283101be937890890716cbb
Dylberto <dylang565@gmail.com>
2018-12-20 23:28:46 +0000

    DONE!!

--- War Never Changes/Assets/Player Stuff/PlayerController.cs
+++ War Never Changes/Assets/Player Stuff/PlayerController.cs
@@ -6,67 +6,73 @@ public class PlayerController : MonoBehaviour
 {
     public float back_ground = 10;
     public Transform player;
+    public float player_speed = 4;
     private Animator PlayerAnimator;
+    private CharacterController controller;
     private Vector3 reset_player;
-    public bool distguised = false;
-    private float forward;
-
-
-
+    public bool distguised;
 
     void Start()
     {
+
+        controller = GetComponent<CharacterController>();
         PlayerAnimator = GetComponent<Animator>();
-       
+
+
     }
 
 
     void Update()
     {
-        PlayerAnimator.SetFloat("Forward", forward);
-        forward = Input.GetAxis("Horizontal");
+
         //Movement
-        Vector3 move = new Vector3(forward, 0, 0);
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        controller.SimpleMove(move * player_speed);
+        move = (transform.forward);
+
+
+
+       
+
 
 
         //the sneak mechanic
         if (Input.GetKeyDown(KeyCode.W))
         {
+            controller.enabled = !controller.enabled;
+            player.Translate(0, 0, -back_ground);
             PlayerAnimator.SetBool("Hiding", true);
         }
+        
 
         if (Input.GetKeyUp(KeyCode.W))
         {
+            controller.enabled = !controller.enabled;
             PlayerAnimator.SetBool("Hiding", false);
             reset_player = new Vector3(player.position.x, player.position.y, 0);
             player.position = reset_player;
         }
 
 
-       
-        //Changing color & distguised
+
+        PlayerAnimator.SetFloat("forward", (Mathf.Abs(Input.GetAxis("Horizontal"))));
+
+
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-            gameObject.GetComponent<Renderer>().material.color = Color.blue;
+
             distguised = true;
-
-            
-         }
-
-        if (Input.GetKeyDown(KeyCode.E) && distguised == true)
-        {
-
-            gameObject.GetComponent<Renderer>().material.color = Color.green;
-
-            //distguised = false;
-
-
         }
 
-
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            distguised = false;
+        }
 
 
     }
 
+
+   
 }
